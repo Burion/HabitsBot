39ba702e628bfdef444909e139d368d5d9f5ce42
Burion <vladislavburyak00@gmail.com>
2021-11-04 18:05:45 +0200

    [+] StringExtensions extensions class

---
+++ HabitsBot.Tests/Helpers/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Tests.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string Shield(this string text, string shield)
+        {
+            var sb = new StringBuilder(text);
+            sb.Insert(0, shield);
+            sb.Append(shield);
+
+            return sb.ToString();
+        }
+
+        public static string SingleQuotesShield(this string text) => text.Shield("'");
+        public static string DoubleQuotesShield(this string text) => text.Shield("\"");
+    }
+}
