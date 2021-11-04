8b5a11fe234c590064942375865b02e5dc67e4b4
Burion <vladislavburyak00@gmail.com>
2021-11-04 18:10:42 +0200

    [+] ConnectionTests.cs - connection tests

---
+++ HabitsBot.Tests/ConnectionTests.cs
@@ -0,0 +1,24 @@
+﻿using HabitsBot.DAL;
+using HabitsBot.DAL.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HabitsBot.Tests
+{
+    public class ConnectionTests
+    {
+        [Fact]
+        public void VerifyConnection()
+        {
+            ConfigHandler.GetJsonConfigurationFromPath("appSettings.json");
+
+            var dataAccesser = new DataAccesser();
+
+            dataAccesser.ExecuteQuery("SELECT * FROM Habits");
+        }
+
+
+    }
+}
