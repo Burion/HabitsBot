6d5cf79fb00629e97d68c6e24c346c66a3b6819c
Burion <vladislavburyak00@gmail.com>
2021-11-04 18:24:33 +0200

    [~] HabitsTests.cs - FIX test for editing

--- HabitsBot.Tests/HabitsTests.cs
+++ HabitsBot.Tests/HabitsTests.cs
@@ -16,6 +16,9 @@ namespace HabitsBot.Tests
         private const string userId = "USER_ID_1";
         private const string habitName = "USER_ID_1";
 
+        private const string userIdEdited = "USER_ID_2";
+        private const string habitNameEdited = "USER_ID_2";
+
         public HabitsTests()
         {
             dataAccesser = new DataAccesser();
@@ -44,8 +47,11 @@ namespace HabitsBot.Tests
         {
             var queryToInsert = $"INSERT INTO Habits (UserId, Name) VALUES ({userId.SingleQuotesShield()}, {habitName.SingleQuotesShield()})";
             dataAccesser.ExecuteQuery(queryToInsert);
+            
+            var queryToEdit = $"UPDATE Habits SET Name={habitNameEdited.SingleQuotesShield()}, UserId={userIdEdited.SingleQuotesShield()} WHERE Name={habitName.SingleQuotesShield()} AND UserId={userId.SingleQuotesShield()}";
+            dataAccesser.ExecuteQuery(queryToEdit);
 
-            var queryToSelect = $"SELECT * FROM Habits WHERE Name={habitName.SingleQuotesShield()} AND UserId={userId.SingleQuotesShield()}";
+            var queryToSelect = $"SELECT * FROM Habits WHERE Name={habitNameEdited.SingleQuotesShield()} AND UserId={userIdEdited.SingleQuotesShield()}";
             var habits = dataAccesser.ExecuteQuery<Habit>(queryToSelect);
 
             if (habits.Count() != 1)
@@ -53,12 +59,15 @@ namespace HabitsBot.Tests
                 Assert.True(false);
             }
 
-            var correctHabit = new Habit() { Name = habitName, UserId = userId };
+            var correctHabit = new Habit() { Name = habitNameEdited, UserId = userIdEdited };
             Assert.Equal(habits.First(), correctHabit);
         }
 
         public void Dispose()
         {
+            var queryToEdit = $"UPDATE Habits SET Name={habitName.SingleQuotesShield()}, UserId={userId.SingleQuotesShield()} WHERE Name={habitNameEdited.SingleQuotesShield()} AND UserId={userIdEdited.SingleQuotesShield()}";
+            dataAccesser.ExecuteQuery(queryToEdit);
+
             var queryToDelete = $"DELETE FROM Habits WHERE Name={habitName.SingleQuotesShield()} AND UserId={userId.SingleQuotesShield()}";
             dataAccesser.ExecuteQuery(queryToDelete);
         }
