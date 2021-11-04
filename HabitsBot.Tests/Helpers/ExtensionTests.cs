0bb248e179fe4b9fd20155dd38a48bb160911134
Burion <vladislavburyak00@gmail.com>
2021-11-04 18:06:04 +0200

    [+] tests for string extensions

---
+++ HabitsBot.Tests/Helpers/ExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HabitsBot.Tests.Helpers
+{
+    public class ExtensionTests
+    {
+        [Fact]
+        public void ShieldTest()
+        {
+            var line = "Some text";
+
+            var shielded = line.Shield("'");
+
+            var shieldedCorrect = "'Some text'";
+
+            Assert.Equal(shielded, shieldedCorrect);
+        }
+    }
+}
