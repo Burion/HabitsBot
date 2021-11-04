04b69066db98e776c90ebc38cf1aec9a01a3b308
Burion <vladislavburyak00@gmail.com>
2021-11-04 14:47:29 +0200

    [init]

---
+++ HabitsBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace HabitsBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
