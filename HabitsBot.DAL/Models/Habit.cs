29ac168d576cb0537f16801d6f0462f3efaec874
Burion <vladislavburyak00@gmail.com>
2021-11-04 14:47:50 +0200

    [+] habit model

---
+++ HabitsBot.DAL/Models/Habit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.DAL.Models
+{
+    public class Habit
+    {
+        public string Name { get; set; }
+        public string UserId { get; set; }
+    }
+}
