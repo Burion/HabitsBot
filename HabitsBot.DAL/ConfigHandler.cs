c4442fc7d0afff824bb0d34bdbddb63799d00dfa
Burion <vladislavburyak00@gmail.com>
2021-11-04 15:59:24 +0200

    [+] config handler

---
+++ HabitsBot.DAL/ConfigHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+
+namespace HabitsBot.DAL
+{
+    public static class ConfigHandler
+    {
+        public static IConfigurationRoot GetJsonConfigurationFromPath(string path)
+        {
+            var configBuilder = new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory).AddJsonFile("appSettings.json");
+            var config = configBuilder.Build();
+
+            return config;
+        }
+    }
+}
