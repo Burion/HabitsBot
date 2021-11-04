38ed8e1ea178f166034d1ca027ce7685bce3d064
Burion <vladislavburyak00@gmail.com>
2021-11-04 18:10:16 +0200

    [~] Habits.cs - override Equals() and GetHashCode()

--- HabitsBot.DAL/Models/Habit.cs
+++ HabitsBot.DAL/Models/Habit.cs
@@ -8,5 +8,17 @@ namespace HabitsBot.DAL.Models
     {
         public string Name { get; set; }
         public string UserId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var habitToCompare = (Habit)obj;
+
+            return habitToCompare.Name == Name && habitToCompare.UserId == UserId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, UserId);
+        }
     }
 }
