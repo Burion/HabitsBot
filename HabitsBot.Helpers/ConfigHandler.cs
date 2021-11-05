f7e7cbe8b440e9953ef80d2a2fc4acbadb2b0434
Burion <vladislavburyak00@gmail.com>
2021-11-05 14:24:00 +0200

    [->] ConfigHandler - to helpers project

--- HabitsBot.Helpers/ConfigHandler.cs
+++ HabitsBot.Helpers/ConfigHandler.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 
-namespace HabitsBot.DAL
+namespace HabitsBot.Helpers
 {
     public static class ConfigHandler
     {
+        private const string configPath = "appSettingsSample.json";
         public static IConfigurationRoot GetJsonConfigurationFromPath(string path)
         {
-            var configBuilder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appSettings.json");
+            var configBuilder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile(configPath);
             var config = configBuilder.Build();
 
             return config;
