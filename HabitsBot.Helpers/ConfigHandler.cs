c25cf69bb98773341b238dd65b3ab4bcad85aa7a
Burion <vladislavburyak00@gmail.com>
2021-11-05 15:17:08 +0200

    [~] ConfigHandler.cs - FIX config handler default path usage

--- HabitsBot.Helpers/ConfigHandler.cs
+++ HabitsBot.Helpers/ConfigHandler.cs
@@ -8,10 +8,12 @@ namespace HabitsBot.Helpers
         private const string configPath = "appSettingsSample.json";
         public static IConfigurationRoot GetJsonConfigurationFromPath(string path)
         {
-            var configBuilder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile(configPath);
+            var configBuilder = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile(path);
             var config = configBuilder.Build();
 
             return config;
         }
+
+        public static IConfigurationRoot GetJsonConfiguration() => GetJsonConfigurationFromPath(configPath);
     }
 }
