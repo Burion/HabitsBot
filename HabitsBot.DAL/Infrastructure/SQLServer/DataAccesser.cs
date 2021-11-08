3efee05df6b0ffa18e9a632f0c07f038fdb0d093
Burion <vladislavburyak00@gmail.com>
2021-11-08 13:24:02 +0200

    [rfc] DataAccesser - extract connection string

--- HabitsBot.DAL/Infrastructure/SQLServer/DataAccesser.cs
+++ HabitsBot.DAL/Infrastructure/SQLServer/DataAccesser.cs
@@ -3,20 +3,26 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using Dapper;
 using System.Collections.Generic;
+using HabitsBot.Helpers;
 
 namespace HabitsBot.DAL.Infrastructure
 {
     public class DataAccesser
     {
         private readonly IConfigurationRoot _config;
+        private readonly string _connectionString;
         public DataAccesser() 
         {
             _config = ConfigHandler.GetJsonConfigurationFromPath("appSettings.json");
+            var connectionStrings = _config.GetSection("connectionStrings");
+            var connString = connectionStrings.GetSection("SQLServer").Value;
+
+            _connectionString = connString;
         }
 
         public IEnumerable<T> ExecuteQuery<T>(string query)
         {
-            using (IDbConnection dbConnection = new SqlConnection(_config.GetSection("connectionString").Value))
+            using (IDbConnection dbConnection = new SqlConnection(_connectionString))
             {
                 var result = dbConnection.Query<T>(query);
                 
@@ -26,7 +32,7 @@ namespace HabitsBot.DAL.Infrastructure
 
         public void ExecuteQuery(string query)
         {
-            using (IDbConnection dbConnection = new SqlConnection(_config.GetSection("connectionString").Value))
+            using (IDbConnection dbConnection = new SqlConnection(_connectionString))
             {
                 var result = dbConnection.Query(query);
             }
