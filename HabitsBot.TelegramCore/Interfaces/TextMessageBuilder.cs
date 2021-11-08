ead90ad2548a82575b4c6f45149d3c4f0a35da22
Burion <vladislavburyak00@gmail.com>
2021-11-08 13:57:16 +0200

    [+] ITextMessageBuilder interface

---
+++ HabitsBot.TelegramCore/Interfaces/TextMessageBuilder.cs
@@ -0,0 +1,12 @@
+﻿using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Interfaces
+{
+    interface ITextMessageBuilder
+    {
+        TextMessageModel BuildTextMessage(string message);
+    }
+}
