c64da010dac27ba446be8031132a8ee66958c8c4
Burion <vladislavburyak00@gmail.com>
2021-11-09 14:53:55 +0200

    [+] ItalicTextMessageBuilder

---
+++ HabitsBot.TelegramCore/Infrastructure/ItalicTextMessageBuilder.cs
@@ -0,0 +1,14 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class ItalicTextMessageBuilder : HtmlDecoratedTextMessageBuilder, ITextMessageBuilder
+    {
+        private const string ITALIC_TAG = "<i>";
+
+        public ItalicTextMessageBuilder() : base(ITALIC_TAG) { }
+    }
+}
