df5972f3f229872d9db1ce653b61fffc7a4bf673
Burion <vladislavburyak00@gmail.com>
2021-11-09 14:52:49 +0200

    [+] DefaultTextMessageBuilder

---
+++ HabitsBot.TelegramCore/Infrastructure/DefaultTextMessageBuilder.cs
@@ -0,0 +1,19 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.Models;
+using HabitsBot.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class DefaultTextMessageBuilder : ITextMessageBuilder
+    {
+        public virtual TextMessageModel BuildTextMessage(string message)
+        {
+            var model = new TextMessageModel(message);
+
+            return model;
+        }
+    }
+}
