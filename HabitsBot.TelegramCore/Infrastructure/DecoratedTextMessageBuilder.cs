95e177fbee118ae7b3a0546a75e323330d99d72b
Burion <vladislavburyak00@gmail.com>
2021-11-09 14:53:29 +0200

    [+] DecoratedTextMessageBuilder

---
+++ HabitsBot.TelegramCore/Infrastructure/DecoratedTextMessageBuilder.cs
@@ -0,0 +1,25 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.Models;
+using HabitsBot.Tests.Helpers;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class HtmlDecoratedTextMessageBuilder : DefaultTextMessageBuilder, ITextMessageBuilder
+    {
+        private readonly string _decoration;
+
+        public HtmlDecoratedTextMessageBuilder(string decoration)
+        {
+            _decoration = decoration;
+        }
+
+        public override TextMessageModel BuildTextMessage(string message)
+        {
+            var model = base.BuildTextMessage(message);
+
+            model.Message = model.Message.ShieldHtmlTag(_decoration);
+
+            return model;
+        }
+    }
+}
