cde86db57cf9411e2b86b5f35abde6d68f4fb75b
Burion <vladislavburyak00@gmail.com>
2021-11-09 16:14:14 +0200

    [+] MessageSummary class

---
+++ HabitsBot.TelegramCore/Models/MessageSummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Types;
+
+namespace HabitsBot.TelegramCore.Models
+{
+    public class MessageSummary
+    {
+        public string Message { get; set; }
+        public ChatId ChatId { get; set; }
+    }
+}
