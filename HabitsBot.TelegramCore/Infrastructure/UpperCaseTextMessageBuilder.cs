8a8c8572d3722801d9de46785e2b11bd06529abe
Burion <vladislavburyak00@gmail.com>
2021-11-09 16:13:34 +0200

    [+] UpperCaseTextMessageBuilder

---
+++ HabitsBot.TelegramCore/Infrastructure/UpperCaseTextMessageBuilder.cs
@@ -0,0 +1,18 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class UpperCaseTextMessageBuilder : DefaultTextMessageBuilder, ITextMessageBuilder
+    {
+        public override string BuildTextMessage(string message)
+        {
+            var messageToReturn = base.BuildTextMessage(message);
+            
+            return messageToReturn.ToUpper(); 
+        }
+    }
+}
