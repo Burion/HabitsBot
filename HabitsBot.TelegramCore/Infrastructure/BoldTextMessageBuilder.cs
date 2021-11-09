759c0146ec2383a420f3672de22478a302021f52
Burion <vladislavburyak00@gmail.com>
2021-11-09 14:53:42 +0200

    [+] BoldTextMessageBuilder

---
+++ HabitsBot.TelegramCore/Infrastructure/BoldTextMessageBuilder.cs
@@ -0,0 +1,16 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.Models;
+using HabitsBot.Tests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class BoldTextMessageBuilder : HtmlDecoratedTextMessageBuilder, ITextMessageBuilder
+    {
+        private const string BOLD_TAG = "<b>";
+
+        public BoldTextMessageBuilder() : base(BOLD_TAG) { }
+    }
+}
