5e16f43264f5d28eca8aca931873bbe40e6b670d
Burion <vladislavburyak00@gmail.com>
2021-11-09 14:51:21 +0200

    [+] ShieldHtmlTag to StringExtensions

--- HabitsBot.Helpers/Extensions/StringExtensions.cs
+++ HabitsBot.Helpers/Extensions/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace HabitsBot.Tests.Helpers
 {
@@ -11,6 +12,29 @@ namespace HabitsBot.Tests.Helpers
             var sb = new StringBuilder(text);
             sb.Insert(0, shield);
             sb.Append(shield);
+
+            return sb.ToString();
+        }
+
+        public static string ShieldHtmlTag(this string text, string shield)
+        {
+            var isTag = Regex.IsMatch(shield, "<[a-zA-Z]>");
+
+            if (!isTag)
+            {
+                throw new ArgumentException("Given value is not HTML tag");
+            }
+
+            var open = shield;
+
+            var openingSymbol = shield.Substring(0, 1);
+            var close = new StringBuilder(openingSymbol);
+            close.Append('/');
+            close.Append(shield.Substring(1, shield.Length - 1));
+
+            var sb = new StringBuilder(text);
+            sb.Insert(0, open);
+            sb.Append(close);
 
             return sb.ToString();
         }
