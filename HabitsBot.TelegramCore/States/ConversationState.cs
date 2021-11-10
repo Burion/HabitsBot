262bacbadbaa05d89ec34e8b7f10dac24f092cb0
Burion <vladislavburyak00@gmail.com>
2021-11-10 17:14:41 +0200

    [+] ConversationState abstract class

---
+++ HabitsBot.TelegramCore/States/ConversationState.cs
@@ -0,0 +1,17 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Args;
+
+namespace HabitsBot.TelegramCore.States
+{
+    public abstract class ConversationState
+    {
+        private readonly Dictionary<string, Action<IConversationDirector, MessageEventArgs>> _handlers;
+
+        public abstract void AddHandler(string key, Action<IConversationDirector, MessageEventArgs> handler);
+        public abstract void DeleteHandler(string key, Action<IConversationDirector, MessageEventArgs> handler);
+        public abstract Action ExecuteState(IConversationDirector director, MessageEventArgs arguments);
+    }
+}
