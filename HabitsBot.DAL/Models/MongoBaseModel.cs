26adffd660a8d3714f854fc5d446ae335dde8afb
Burion <vladislavburyak00@gmail.com>
2021-11-10 17:13:14 +0200

    [+] Mongo Base model

---
+++ HabitsBot.DAL/Models/MongoBaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.DAL.Models
+{
+    public class MongoBaseModel
+    {
+        public string Id { get; set; }
+    }
+}
