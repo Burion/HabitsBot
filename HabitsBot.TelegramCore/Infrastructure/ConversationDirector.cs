462e2241aa986b9b5b1a722e758a064161839964
Burion <vladislavburyak00@gmail.com>
2021-11-10 17:14:57 +0200

    [+] I-ConversationDirector

---
+++ HabitsBot.TelegramCore/Infrastructure/ConversationDirector.cs
@@ -0,0 +1,13 @@
+﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public class ConversationDirector : IConversationDirector
+    {
+        public IConversationState CurrentState { get; set; }
+    }
+}
---
+++ HabitsBot.TelegramCore/Interfaces/IConversationDirector.cs
@@ -0,0 +1,12 @@
+﻿using HabitsBot.TelegramCore.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.Interfaces
+{
+    public interface IConversationDirector
+    {
+        public IConversationState CurrentState { get; set; }
+    }
+}
