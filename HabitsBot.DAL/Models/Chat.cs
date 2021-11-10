e8f70eb6f8e43d3a91b108e9b702dfd3850c589f
Burion <vladislavburyak00@gmail.com>
2021-11-10 17:14:00 +0200

    [~] Chat model - inherit base class

---
+++ HabitsBot.DAL/Models/Chat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.DAL.Models
+{
+    public class Chat : MongoBaseModel
+    {
+        public int State { get; set; }
+        public string UserId { get; set; }
+    }
+}
