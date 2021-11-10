bfb3c5e8becb5b566ca2fcb760f3921ffc552055
Burion <vladislavburyak00@gmail.com>
2021-11-10 17:12:56 +0200

    [+] IChatAccesser

---
+++ HabitsBot.DAL/Interfaces/IChatsAccesser.cs
@@ -0,0 +1,17 @@
+﻿using HabitsBot.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.DAL.Interfaces
+{
+    interface IChatsAccesser
+    {
+        Chat AddChat(Chat model);
+        Chat DeleteChat(Chat model);
+        Chat DeleteChatOrNull(Chat model);
+        Chat EditChat(Chat model);
+        Chat GetChat(Chat model);
+        Chat GetChatOrNull(Chat model);
+    }
+}
