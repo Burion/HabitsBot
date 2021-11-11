e263ff157ac18057e6308f2f4ec4d22f68052e7d
Burion <vladislavburyak00@gmail.com>
2021-11-11 20:03:15 +0200

    [+] MessageHandleType - enum for different message handling branching

---
+++ HabitsBot.TelegramCore/States/Configurations/MessageHandleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.TelegramCore.States.Configurations
+{
+    public enum MessageHandleType
+    {
+        Default = 1,
+        FreeText = 2,
+        Error = 3
+    }
+}
