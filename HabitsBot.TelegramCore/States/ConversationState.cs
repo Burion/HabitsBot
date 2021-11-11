6d21d3e00954c244dd61b6dbc26167769fae9d15
Burion <vladislavburyak00@gmail.com>
2021-11-11 20:03:59 +0200

    [+] ConversationState - methods and props for specific handlers

--- HabitsBot.TelegramCore/States/ConversationState.cs
+++ HabitsBot.TelegramCore/States/ConversationState.cs
@@ -1,6 +1,7 @@
 ﻿using HabitsBot.TelegramCore.Infrastructure;
 using HabitsBot.TelegramCore.Interfaces;
 using HabitsBot.TelegramCore.Models;
+using HabitsBot.TelegramCore.States.Configurations;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,7 +19,10 @@ namespace HabitsBot.TelegramCore.States
         protected readonly Dictionary<string, Action<ConversationDirector, MessageDto>> _handlers;
 
         public abstract void AddHandler(string key, Action<ConversationDirector, MessageDto> handler);
+        public abstract void AddHandler(Action<ConversationDirector, MessageDto> handler, MessageHandleType type);
         public abstract void DeleteHandler(string key);
         public abstract Action ExecuteState(ConversationDirector director, MessageDto message);
+        protected Action<ConversationDirector, MessageDto> FreeMessageHandler { get; set; }
+        protected Action<ConversationDirector, MessageDto> ErrorHandler { get; set; }
     }
 }
