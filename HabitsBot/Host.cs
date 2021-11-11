80600398f8cc266b544f44b7698d14e33dd7645e
Burion <vladislavburyak00@gmail.com>
2021-11-11 18:33:33 +0200

    [+] Host static class

---
+++ HabitsBot/Host.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot
+{
+    public static class Host<T> where T: StartupBase
+    {
+        public static void Run()
+        {
+            T startup = (T)Activator.CreateInstance(typeof(T));
+
+            startup.RunApplication();
+        }
+    }
+}
