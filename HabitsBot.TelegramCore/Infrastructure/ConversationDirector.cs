88ffd4e354eee62c7b752abf53c33728b6425bd8
Burion <vladislavburyak00@gmail.com>
2021-11-11 18:27:34 +0200

    [+] ConversationDirector class

--- HabitsBot.TelegramCore/Infrastructure/ConversationDirector.cs
+++ HabitsBot.TelegramCore/Infrastructure/ConversationDirector.cs
@@ -1,13 +1,38 @@
 ﻿using HabitsBot.TelegramCore.Interfaces;
+using HabitsBot.TelegramCore.Models;
 using HabitsBot.TelegramCore.States;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Telegram.Bot;
 
 namespace HabitsBot.TelegramCore.Infrastructure
 {
-    public class ConversationDirector : IConversationDirector
+    public class ConversationDirector
     {
-        public IConversationState CurrentState { get; set; }
+        internal TelegramBotClient BotClient { get; }
+        internal ConversationState CurrentState { get; set; }
+
+        public ConversationDirector(string botToken)
+        {
+            BotClient = new TelegramBotClient(botToken);
+        }
+
+        public void SetState(ConversationState state)
+        {
+            CurrentState = state;
+        }
+
+        public void InitializeReceiving()
+        {
+            BotClient.StartReceiving();
+
+            BotClient.OnMessage += (e, a) => CurrentState.ExecuteState(this, new MessageDto(a.Message))();
+        }
+
+        public void ShutDown()
+        {
+            BotClient.StopReceiving();
+        }
     }
 }
