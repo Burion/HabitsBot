2c3dfb2130cbfd328e6444620af6faa3c698af49
Burion <vladislavburyak00@gmail.com>
2021-11-11 18:33:14 +0200

    [+] Startup abstract class

---
+++ HabitsBot/StartupBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot
+{
+    public abstract class StartupBase
+    {
+        public StartupBase()
+        {
+
+        }
+
+        public abstract void RunApplication();
+    }
+}
