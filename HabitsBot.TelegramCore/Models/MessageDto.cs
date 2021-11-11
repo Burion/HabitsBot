0ed3439830d1c30e797cb6e0bbb67c669b78c448
Burion <vladislavburyak00@gmail.com>
2021-11-11 18:29:44 +0200

    [+] MessageDto

---
+++ HabitsBot.TelegramCore/Models/MessageDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Types;
+
+namespace HabitsBot.TelegramCore.Models
+{
+    public class MessageDto : Message
+    {
+    }
+}
