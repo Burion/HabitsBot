9f8c3f98e9b08f83f7efedcf8fd4361fd84c2587
Burion <vladislavburyak00@gmail.com>
2021-11-12 15:28:17 +0200

    [+] IChatService - protocol or chat services

---
+++ HabitsBot.Services/Interfaces/IChatService.cs
@@ -0,0 +1,19 @@
+﻿using HabitsBot.DAL.Models;
+using HabitsBot.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Services.Interfaces
+{
+    public interface IChatService
+    {
+        ChatDto AddChat(ChatDto chat);
+        ChatDto DeleteChat(ChatDto chat);
+        ChatDto EditChat(ChatDto chat);
+        ChatDto GetChat(ChatDto chat);
+        ChatDto GetChatById(string id);
+        ChatDto GetChatByUserId(string id);
+
+    }
+}
