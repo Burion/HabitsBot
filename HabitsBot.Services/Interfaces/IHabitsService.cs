0d22537aa209676a1f148cf430c1fe62472b367a
Burion <vladislavburyak00@gmail.com>
2021-11-12 15:28:34 +0200

    [+] IHabitsService - protocol for habits services

---
+++ HabitsBot.Services/Interfaces/IHabitsService.cs
@@ -0,0 +1,16 @@
+﻿using HabitsBot.Services.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Services.Interfaces
+{
+    public interface IHabitsService
+    {
+        HabitDto AddHabit(HabitDto habit);
+        HabitDto DeleteHabit(HabitDto habit);
+        HabitDto EditHabit(HabitDto habit);
+        HabitDto GetHabitById(string id);
+        IEnumerable<HabitDto> GetHabitsForChat(string chatId);
+    }
+}
