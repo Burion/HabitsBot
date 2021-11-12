d15604b76e16a27cc2ff3a28eff27f1cc521f80e
Burion <vladislavburyak00@gmail.com>
2021-11-12 15:27:40 +0200

    [+] HabitDto - Dto for habits

---
+++ HabitsBot.Services/Dtos/HabitDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Services.Dtos
+{
+    public class HabitDto
+    {
+        public string Name { get; set; }
+        public string ChatId { get; set; }
+    }
+}
