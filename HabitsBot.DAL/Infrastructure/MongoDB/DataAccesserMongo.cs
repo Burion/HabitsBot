17f8abf8409eb1fa6d95c48d7965a3fb8c5c33ae
Burion <vladislavburyak00@gmail.com>
2021-11-12 15:23:02 +0200

    [~] DataAccesserMongo - FIX id issue

--- HabitsBot.DAL/Infrastructure/MongoDB/DataAccesserMongo.cs
+++ HabitsBot.DAL/Infrastructure/MongoDB/DataAccesserMongo.cs
@@ -26,6 +26,7 @@ namespace HabitsBot.DAL.Infrastructure.MongoDB
 
         public T AddItem(T model)
         {
+            model.Id = model.GetHashCode().ToString();
             itemsCollection.InsertOne(model);
 
             return model;
