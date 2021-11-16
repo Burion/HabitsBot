5a5bb70c80adc67fe7ba2d0375b89029e825a9f6
Burion <vladislavburyak00@gmail.com>
2021-11-16 16:15:05 +0200

    [+] AddHabitHandler

---
+++ HabitsBot/Custom/AddHabitHandler.cs
@@ -0,0 +1,36 @@
+﻿using HabitsBot.Services.ClientServices;
+using HabitsBot.Services.Dtos;
+using HabitsBot.Services.Interfaces;
+using HabitsBot.TelegramCore.Infrastructure;
+using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Custom
+{
+    public class AddHabitHandler : Handler
+    {
+        public override void ExecuteHandler(ConversationDirector director, MessageDto message)
+        {
+            IHabitsService habitsService = new HabitsService();
+            IChatService chatService = new ChatService();
+
+            var habit = new HabitDto()
+            {
+                ChatId = message.Chat.Id.ToString(),
+                Name = message.Text
+            };
+
+            habitsService.AddHabit(habit);
+
+            var chat = chatService.GetChatById(message.Chat.Id.ToString());
+            chat.State = 0;
+            chatService.EditChat(chat);
+
+            MessageComposer messageComposer = new MessageComposer(director, new MessageSummary() { ChatId = message.Chat.Id, Message = $"\"{habit.Name}\" is successfully added to your habits pool." });
+            var regular = new ItalicTextMessageBuilder();
+            messageComposer.ComposeMessageDelegate(regular)();
+        }
+    }
+}
