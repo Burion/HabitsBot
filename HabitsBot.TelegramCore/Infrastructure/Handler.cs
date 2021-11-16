08f3738dde09b7f65002f9ee63ba152224d0151d
Burion <vladislavburyak00@gmail.com>
2021-11-16 13:54:53 +0200

    [+] Handler class

---
+++ HabitsBot.TelegramCore/Infrastructure/Handler.cs
@@ -0,0 +1,13 @@
+﻿using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Types;
+
+namespace HabitsBot.TelegramCore.Infrastructure
+{
+    public abstract class Handler
+    {
+        public abstract void ExecuteHandler(ConversationDirector director, MessageDto message);
+    }
+}
