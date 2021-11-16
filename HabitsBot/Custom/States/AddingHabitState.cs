b4a85fcaaed35ab0f668b5e5ce396f37324ce259
Burion <vladislavburyak00@gmail.com>
2021-11-16 16:15:53 +0200

    [+] AddingHabitState impl

---
+++ HabitsBot/Custom/States/AddingHabitState.cs
@@ -0,0 +1,16 @@
+﻿using HabitsBot.TelegramCore.States;
+using HabitsBot.TelegramCore.States.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Custom.States
+{
+    public class AddingHabitState : DefaultConversationState
+    {
+        public AddingHabitState()
+        {
+            AddHandler(new AddHabitHandler(), MessageHandleType.FreeText);
+        }
+    }
+}
