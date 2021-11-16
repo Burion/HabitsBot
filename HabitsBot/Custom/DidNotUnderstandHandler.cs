7a36f8a4a949531914bf740e43bcb8543b6fc057
Burion <vladislavburyak00@gmail.com>
2021-11-16 13:58:26 +0200

    [+] DidNotUnterstandHandler - Handler impl

---
+++ HabitsBot/Custom/DidNotUnderstandHandler.cs
@@ -0,0 +1,20 @@
+﻿using HabitsBot.TelegramCore.Infrastructure;
+using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Custom
+{
+    public class DidNotUnderstandHandler : Handler
+    {
+        public override void ExecuteHandler(ConversationDirector director, MessageDto message)
+        {
+            MessageComposer messageComposer = new MessageComposer(director, new MessageSummary() { ChatId = message.Chat.Id, Message = "Sorry, I didn't understand your command." });
+
+            var regular = new DefaultTextMessageBuilder();
+
+            messageComposer.ComposeMessageDelegate(regular)();
+        }
+    }
+}
