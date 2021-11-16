fe8df9a04005a360f19749fa35b66578ccef2a77
Burion <vladislavburyak00@gmail.com>
2021-11-16 16:15:31 +0200

    [+] MoveToAddHabitHandler

---
+++ HabitsBot/Custom/MoveToAddHabitHandler.cs
@@ -0,0 +1,28 @@
+﻿using HabitsBot.Services.ClientServices;
+using HabitsBot.Services.Interfaces;
+using HabitsBot.TelegramCore.Infrastructure;
+using HabitsBot.TelegramCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Custom
+{
+    public class MoveToAddHabitHandler : Handler
+    {
+        public override void ExecuteHandler(ConversationDirector director, MessageDto message)
+        {
+            IChatService chatService = new ChatService();
+            
+            var chat = chatService.GetChatById(message.Chat.Id.ToString());
+            chat.State = 1;
+            
+            chatService.EditChat(chat);
+
+            var _message = "Please, enter name for new habit:";
+            MessageComposer messageComposer = new MessageComposer(director, new MessageSummary() { ChatId = message.Chat.Id, Message = _message });
+            var regular = new DefaultTextMessageBuilder();
+            messageComposer.ComposeMessageDelegate(regular)();
+        }
+    }
+}
