f37c2ae4f1594d017cd7da703e5f72642bc3e512
Burion <vladislavburyak00@gmail.com>
2021-11-16 16:15:43 +0200

    [+] StartState impl

---
+++ HabitsBot/Custom/States/StartState.cs
@@ -0,0 +1,18 @@
+﻿using HabitsBot.TelegramCore.States;
+using HabitsBot.TelegramCore.States.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HabitsBot.Custom.States
+{
+    public class StartState : DefaultConversationState 
+    {
+        public StartState()
+        {
+            AddHandler("/start", new StartHandler());
+            AddHandler("/add_habit", new MoveToAddHabitHandler());
+            AddHandler(new DidNotUnderstandHandler(), MessageHandleType.Error);
+        }
+    }
+}
